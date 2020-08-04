7d49ba6b72ec072e6efa048c7de00836fe2e046a
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-04 16:42:57 +0545

    files uploaded

---
+++ todo.js
@@ -0,0 +1,44 @@
+const body = document.querySelector('#body');
+const myform = document.querySelector('#myform');
+const userinput = document.querySelector('#userinput');
+const btn = document.querySelector('#btn');
+
+const msg = document.querySelector('#msg');
+
+const list = document.querySelector('#list');
+
+myform.addEventListener('submit', onsubmit);
+
+
+function onsubmit(s){
+    s.preventDefault();
+    if(userinput.value === ''){
+        // msg.innerHTML = ("Enter Something!");
+        // msg.classList.add('msg');
+
+        // setTimeout(() => msg.remove(), 2000 );
+        alert('Enter something');
+
+    }
+    else{
+        const li = document.createElement('li');
+        const remove = document.createElement('div');
+        li.appendChild(document.createTextNode(`${userinput.value}`));
+        remove.appendChild(document.createTextNode('Delete'));
+        remove.classList.add('remove');
+
+        list.appendChild(li);
+        li.appendChild(remove);
+
+        userinput.value = '';
+
+        remove.addEventListener('click', function(r){
+            li.remove();
+        })
+
+        li.addEventListener('click', function(c){
+            li.classList.toggle('li-check');
+        })
+    }
+
+}
